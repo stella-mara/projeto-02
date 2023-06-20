2e8acf5fa6b673f68c30830a3481f0c27d232dbc
Stella <stellamarawendt@gmail.com>
2023-06-20 15:09:35 -0300

    solucionando erros

--- Services/Interfaces/IUsuariosService.cs
+++ Services/Interfaces/IUsuariosService.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using projeto_02.Models;
 using projeto_02.Models.Enum;
 using projeto_02.Models.ViewModels;
 
 namespace projeto_02.Services.Interfaces
 {
-    public class IUsuariosService
+    public interface IUsuariosService
     {
         Task<bool?> CreateAsync(PostUsuario usuario);
         Task<bool?> UpdateAsync(PutUsuario usuario);
--- Services/UsuariosService.cs
+++ Services/UsuariosService.cs
@@ -2,12 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using projeto_02.Models.Enum;
+using projeto_02.Models.ViewModels;
 using projeto_02.Services.Interfaces;
 
 namespace projeto_02.Services
 {
-    public class UsuariosService : IUsuariosService
+  public class UsuariosService : IUsuariosService
+  {
+    public Task<bool?> CreateAsync(PostUsuario usuario)
     {
-        
+      throw new NotImplementedException();
     }
+
+    public Task<bool?> UpdateAsync(PutUsuario usuario)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool?> UpdateStatusAsync(int id, Status status)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
